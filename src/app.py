1cb582f329bd4aea3e62a08294b071d076d30657
Thiago Penha <thiagoppc@al.insper.edu.br>
2025-05-22 11:30:55 -0300

    Deploy Heroku

--- src/app.py
+++ src/app.py
@@ -16,31 +16,39 @@ app = FastAPI()
 def test():
     return {"status": "OK v2 (3)"}
 
+allowed_origins = [
+    "http://localhost:5173",
+    "https://seu-frontend.herokuapp.com",
+    "https://seu-app.herokuapp.com"
+]
+
+if os.getenv("ENVIRONMENT") == "production":
+    allowed_origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],          
+    allow_origins=allowed_origins,          
     allow_credentials=True,                  
     allow_methods=["*"],                     
     allow_headers=["*"],                     
 )
 
 working_directory = os.path.dirname(os.path.abspath(__file__))
-
 use_cases_directory = os.path.join(working_directory, "use_cases")
-
 routes = glob.glob(os.path.join(use_cases_directory, "**/index.py"), recursive=True)
 
 for route in routes:
-
     relative_path = os.path.relpath(route, working_directory)
-
     module_name = os.path.splitext(relative_path)[0].replace(os.path.sep, '.')
-
+    
     try:
         module = import_module(module_name)
-
         if hasattr(module, 'router'):
             app.include_router(module.router)
-
     except ModuleNotFoundError as e:
         print(f"Erro ao importar módulo {module_name}: {e}")
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 5000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
